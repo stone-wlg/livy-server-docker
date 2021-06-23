020e2376a065d66bbfcbde580571e7a88e982ada
wliang3 <stone_wlg@163.com>
2021-06-23 16:04:43 +0800

    ...

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-docker build -t stonewlg/livy:0.7.1-spark2.4.8 .
+docker build -t stonewlg/livy-server:0.7.1-spark2.4.8 .
--- push.sh
+++ push.sh
@@ -2,4 +2,4 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-docker push stonewlg/livy:0.7.1-spark2.4.8
+docker push stonewlg/livy-server:0.7.1-spark2.4.8
